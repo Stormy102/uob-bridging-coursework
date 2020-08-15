78bf210706225096934d4ed9c22696d66b2c3639
Matt <matthewd102@hotmail.com>
2020-08-15 19:38:05 +0100

    Finalised most of website styling

--- blog/static/js/cookie-consent.js
+++ blog/static/js/cookie-consent.js
@@ -2,15 +2,14 @@ window.addEventListener("load", function(){
 window.cookieconsent.initialise({
   "palette": {
     "popup": {
-      "background": "#222"
+      "background": "#303030"
     },
     "button": {
-      "background": "#9acc14"
+      "background": "#222"
     }
   },
   "theme": "edgeless",
   "position": "top",
-  "static": true,
   "content": {
     "message": "This website uses Cookies 🍪 to store data."
   }
--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,15 @@ def index(request):
 
 
 def post_list(request):
+    post_numbers = Post.objects.all().count()
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts, 'post_count': post_numbers})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def handler404(request, exception):
+    return render(request, 'blog/404.html', status=404)
